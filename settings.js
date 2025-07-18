531aea6e55c5b5b6d8a225c4e3dd17c6d9671fb7
MUQEET-MD <quotexmarketview0@gmail.com>
2025-07-18 06:52:54 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=psZEBSTD#oS6c_1UGuZe_JIhwPquPsR8N7gVy61HhegL4T1RBY74",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
